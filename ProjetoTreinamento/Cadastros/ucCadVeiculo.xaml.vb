f102d976b06fb0c28e9523fd78cf8523e27e5116
Matheus Gelio <officialmatheusgelio@gmail.com>
2022-08-24 11:03:27 -0300

    Treinamento Sizex (A21)

--- ProjetoTreinamento/Cadastros/ucCadVeiculo.xaml.vb
+++ ProjetoTreinamento/Cadastros/ucCadVeiculo.xaml.vb
@@ -1,3 +1,12 @@
 ﻿Public Class ucCadVeiculo
 
+    Private Sub ucCadVeiculo_Loaded(sender As Object, e As RoutedEventArgs) Handles Me.Loaded
+        Dim lista As New List(Of String)
+        lista.Add("DIESEL")
+        lista.Add("ETANOL")
+        lista.Add("FLEX")
+        lista.Add("GASOLINA")
+
+        CombustivelTxt.ItemsSource = lista.ToList
+    End Sub
 End Class
