8d9e21d2996ab97c9278157bcfa46144b2d1ba34
Matheus Gelio <officialmatheusgelio@gmail.com>
2022-09-02 12:36:54 -0300

    Treinamento Sizex (A32)

---
+++ ProjetoTreinamento/Classes/Produto.vb
@@ -0,0 +1,12 @@
+﻿Public Class Produto
+    Public Property Codigo As Integer
+    Public Property Nome As String
+    Public Property DataCadastro As Date
+    Public Property Estoque As Boolean
+    Public Property Grupo As String
+    Public Property TipoProduto As String
+    Public Property Custo As Double
+    Public Property Margem As Double
+    Public Property Preco As Double
+    Public Property Inativo As Boolean
+End Class
---
+++ ProjetoTreinamento/Classes/Veiculo.vb
@@ -0,0 +1,12 @@
+﻿Public Class Veiculo
+    Public Property Placa As String
+    Public Property DescricaoVeiculo As String
+    Public Property Combustivel As String
+    Public Property UltimoKm As Double
+    Public Property ValorCompra As Double
+    Public Property DataAquisicao As Date
+    Public Property DataAbast As Date
+    Public Property KmAbast As Double
+    Public Property Litros As Double
+    Public Property ValorTotal As Double
+End Class
