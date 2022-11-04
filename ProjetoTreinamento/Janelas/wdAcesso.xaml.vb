53fbe08cb30e860f1560d5805ca04c04009499c0
Matheus Gelio <officialmatheusgelio@gmail.com>
2022-11-04 12:08:54 -0300

    Treinamento Sizex (A48)

---
+++ ProjetoTreinamento/Janelas/wdAcesso.xaml.vb
@@ -0,0 +1,13 @@
+﻿Public Class wdAcesso
+
+    Private Sub SairBtn_Click(sender As Object, e As RoutedEventArgs) Handles SairBtn.Click
+        Me.Close()
+    End Sub
+
+
+    Private Sub wdAcesso_KeyDown(sender As Object, e As KeyEventArgs) Handles Me.KeyDown
+        If e.Key = Key.Escape Then
+            Me.Close()
+        End If
+    End Sub
+End Class
